a5789466115bdb7e8d15fdfda41192a68ac44489
wangjue666 <3262629206@qq.com>
2018-01-25 15:00:42 +0800

    use jade template to render a HTML-page

---
+++ mainJade.js
@@ -0,0 +1,13 @@
+const jade=require('jade');
+const fs=require('fs');
+
+var str=jade.renderFile('./views/index.jade',
+    {
+        pretty:true,
+    });
+fs.writeFile("./www/jade.html",str,function(err){
+    if(err)
+        console.log('编译失败')
+    else
+        console.log('成功')
+})
